ba3237ced1eefb6f659733f3a3c8b19fc0375726
denis <denis@DESKTOP-K3JSVD1>
2024-02-01 15:13:19 +0100

    Добавьте файлы проекта.

---
+++ AddWin.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows;
+
+namespace Oleksandr_Dikhtiarenko_AplikacjaBankowa
+{
+    public partial class AddWin : Window
+    {
+        private MainWindow mainWindow;
+
+        public AddWin(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            this.mainWindow = mainWindow;
+        }
+
+        public bool isValid()
+        {
+            string[] textBoxes = { tbfirst.Text, tblast.Text, tbpesel.Text, tbnumber.Text };
+            string[] fieldNames = { "FirstName", "LastName", "Pesel", "AkaNumber" };
+
+            for (int i = 0; i < textBoxes.Length; i++)
+            {
+                if (string.IsNullOrEmpty(textBoxes[i]))
+                {
+                    MessageBox.Show($"Failed: There are empty or invalid values in the {fieldNames[i]} field.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void SaveBtn(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (isValid())
+                {
+                    using (SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-K3JSVD1;Initial Catalog=Bank;Integrated Security=True;"))
+                    {
+                        connection.Open();
+
+                        using (SqlCommand sqlc = new SqlCommand("INSERT INTO BankAka (FirstName, LastName, Pesel, AkaNumber) VALUES (@FirstName, @LastName, @Pesel, @AkaNumber)", connection))
+                        {
+                            sqlc.CommandType = CommandType.Text;
+                            sqlc.Parameters.AddWithValue("@FirstName", tbfirst.Text);
+                            sqlc.Parameters.AddWithValue("@LastName", tblast.Text);
+                            sqlc.Parameters.AddWithValue("@Pesel", tbpesel.Text);
+                            sqlc.Parameters.AddWithValue("@AkaNumber", tbnumber.Text);
+
+                            sqlc.ExecuteNonQuery();
+                        }
+                    }
+
+                    mainWindow.Load();
+                    MessageBox.Show("Successfully registered", "Saved", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+    }
+}
+
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Oleksandr_Dikhtiarenko_AplikacjaBankowa
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Load();
+        }
+
+        public static SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-K3JSVD1;Initial Catalog=Bank;Integrated Security=True;");
+
+        public void Load()
+        {
+            SqlCommand sqlc = new SqlCommand("select * from BankAka", connection);
+            DataTable dataTable = new DataTable();
+            connection.Open();
+            SqlDataReader sqldr = sqlc.ExecuteReader();
+            dataTable.Load(sqldr);
+            connection.Close();
+            DataGrid.ItemsSource = dataTable.DefaultView;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-K3JSVD1;Initial Catalog=Bank;Integrated Security=True;"))
+                {
+                    connection.Open();
+                    SqlCommand sqlc = new SqlCommand("Delete from BankAka where PeopleId = @PeopleId", connection);
+                    sqlc.Parameters.AddWithValue("@PeopleId", tbSerch.Text);
+
+                    sqlc.ExecuteNonQuery();
+                    MessageBox.Show("Record has been deleted", "Deleted", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                Load(); 
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Not Deleted: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AddWin window = new AddWin(this);
+            window.Show();
+        }
+    }
+}
